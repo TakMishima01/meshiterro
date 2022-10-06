1443e13aeefdef720406d248b070c0490532144a
TakMishima01 <114778285+TakMishima01@users.noreply.github.com>
2022-10-06 22:15:04 +0000

    [Add]4-24bootstrapの追加

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -13,14 +13,8 @@ class PostImagesController < ApplicationController
     end
   end
 
-  def destroy
-    @post_image = PostImage.find(params[:id])
-    @post_image.destroy
-    redirect_to '/post_images'
-  end
-
   def index
-    @post_images = PostImage.page(params[:page])
+    @post_images = PostImage.page(params[:page]).reverse_order
   end
 
   def show
@@ -28,12 +22,15 @@ class PostImagesController < ApplicationController
     @post_comment = PostComment.new
   end
 
+def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to '/post_images'
+end
+
   private
 
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
-
-
-
 end
